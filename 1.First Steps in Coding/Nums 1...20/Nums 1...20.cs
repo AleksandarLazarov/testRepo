b153e124350b11eff69356b7f2780cf2ae8aac7f
AleksandarLazarov <aplazarov@abv.bg>
2017-01-21 17:11:27 +0200

    Update First step

---
+++ 1.First Steps in Coding/Nums 1...20/Nums 1...20.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (var i = 1; i <= 20; i++)
+        {
+            Console.WriteLine(i);
+
+        }
+    }
+}
---
+++ 1.First Steps in Coding/Triangle of 55 Stars/Triangle of 55 Stars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (var i = 1; i <= 10; i++)
+        {
+            for (var ii = 1; ii <= i; ii++)
+            {
+                Console.Write("*");
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
